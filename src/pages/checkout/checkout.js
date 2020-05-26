83686425e1f3c64694280f02fe569f4e782334ab
dougofi <dougyboy14@gmail.com>
2020-05-26 08:55:52 +0000

    adding test stripe integration

--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 
 import CheckoutItem from "../../components/checkout-item/checkout.item";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button";
 import { createStructuredSelector } from "reselect";
 import {
   selectCartItems,
@@ -36,6 +37,13 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/21 CVV: any 3 integers
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
